249f0609ba06080c40af6ed917b47c83882b9473
rauf <abdurauf.dev@gmail.com>
2024-05-16 14:14:48 +0500

    перед написание декоратора

--- avito_account/models.py
+++ avito_account/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+import os
+import requests
+from dotenv import load_dotenv
+from exceptions import HTTPException
+
+load_dotenv()
+client_id = os.getenv('AVITO_CLIENT_ID')
+client_secret = os.getenv('AVITO_CLIENT_SECRET')
 
 
 class AvitoAccount(models.Model):
@@ -10,6 +18,26 @@ class AvitoAccount(models.Model):
     profile_url = models.CharField(max_length=255, null=True)
     access_token = models.CharField(max_length=255, null=True)
     refresh_token = models.CharField(max_length=255, null=True)
+
+    def refresh_token(self):
+        url = 'https://api.avito.ru/token/'
+        data = {
+            'grant_type': 'refresh_token',
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'refresh_token': self.refresh_token
+        }
+
+        response = requests.post(url, data=data)
+        response_data = response.json()
+
+        if response.status_code != 200:
+            raise HTTPException(status_code=response.status_code, detail=response.text)
+        else:
+            self.access_token = response_data['access_token']
+            self.refresh_token = response_data['refresh_token']
+            self.save()
+            return True
 
     def __str__(self):
         return f"{self.name}, {self.id}"
@@ -27,6 +55,3 @@ class Item(models.Model):
 
     def __str__(self):
         return f"{self.title, self.id}"
-
-
-
