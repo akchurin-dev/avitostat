6716cc0bb7118cdae3fca84266023dda9385f9d8
rauf <abdurauf.dev@gmail.com>
2024-05-18 14:59:23 +0500

    sqllite

--- base/settings.py
+++ base/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '172.22.0.2',  # Если контейнер и Django приложение на одной машине
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
