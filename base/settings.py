02c8bcf584cf2c9f6085d1a4d975d96f8d168724
rauf <abdurauf.dev@gmail.com>
2024-05-18 19:48:57 +0500

    postrgres DB

--- base/settings.py
+++ base/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
