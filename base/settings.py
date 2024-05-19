6dd3983f4d66a0b151baab66e8c85604769d7352
rauf <abdurauf.dev@gmail.com>
2024-05-19 12:24:09 +0500

    allowed_hosts

--- base/settings.py
+++ base/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-umx*1bjclxog_*haru1%&$m2injd^_eftaiz2qrbctphhuy(5n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["172.22.0.2", "localhost", "127.0.0.1", "45.12.238.229"]
+ALLOWED_HOSTS = ["172.22.0.2", "localhost", "127.0.0.1", "45.12.238.229", "*"]
 
 # Application definition
 
