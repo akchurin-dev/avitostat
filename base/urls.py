c9e35d7a03e270f802adc2ee94ca26759b0b3862
rauf <abdurauf.dev@gmail.com>
2024-05-20 15:39:18 +0500

    test sentry

--- base/urls.py
+++ base/urls.py
@@ -19,10 +19,19 @@ from django.urls import path, include
 
 import avito_account
 
+# urls.py
+from django.urls import path
+
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("oauth/", include("avito_account.urls")),
     path("conversion/", include("conversion.urls")),
+    path('sentry-debug/', trigger_error),
 ]
 
 # if settings.DEBUG:
