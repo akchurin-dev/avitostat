827beb6c5f8fe708e275480723cd3248f7834597
rauf <abdurauf.dev@gmail.com>
2024-05-20 15:28:25 +0500

    debug false

--- base/settings.py
+++ base/settings.py
@@ -26,7 +26,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["172.22.0.2", "localhost", "127.0.0.1", "45.12.238.229", "*", "avitostata.ru"]
 
