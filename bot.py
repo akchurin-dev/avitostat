4c218c7fac6f36fe53e7ee4d10b060189ce93d78
rauf <abdurauf.dev@gmail.com>
2024-05-22 17:02:30 +0500

    до украшения отчета

--- bot.py
+++ bot.py
@@ -27,20 +27,34 @@ async def get_week_report_text(message: Message):
 
     week_report_data = get_week_report_by_telegram_id(telegram_chat_id=message.chat.id)
     avito_account_name = week_report_data.get("avito_account_name")
+    total_items_count = week_report_data.get("total_metrics").get("total_items_count").get("active")
+    total_contacts_count = week_report_data.get("total_metrics").get("total_contacts_count")
+    total_views_count = week_report_data.get("total_metrics").get("total_views_count")
+    total_favorites_count = week_report_data.get("total_metrics").get("total_favorites_count")
+    total_coast = week_report_data.get("total_metrics").get("total_coast")
+    total_coast_per_contact = week_report_data.get("total_metrics").get("total_coast_per_contact")
     text = (f"Еженедельный отчёт: \n\n"
-            f"Аккаунт - <b>{avito_account_name}</b>\n")
+            f"Аккаунт - <b>{avito_account_name}</b>\n"
+            f"Активных объявлений - <b>{total_items_count}</b>\n"
+            f"Запрошено контактов - <b>{total_contacts_count}</b>\n"
+            f"Просмотров - <b>{total_views_count}</b>\n"
+            f"В Избранное - <b>{total_favorites_count}</b>\n"
+            f"Затраты - <b>{total_coast}</b>\n"
+            f"Цена за контакт- <b>{total_coast_per_contact}</b>\n"
+
+            )
 
     statistics_total = ""
-    conversions = week_report_data.get("conversions")
-    if conversions is not None:
-        for item in conversions:
-            itemTitle = conversions.get(item).get("itemTitle", 0)
-            uniqContacts = conversions.get(item).get("uniqContacts", 0)
-            uniqFavorites = conversions.get(item).get("uniqFavorites", 0)
-            uniqViews = conversions.get(item).get("uniqViews", 0)
-            coast = conversions.get(item).get("coast", 0)
-            amount_per_contact = conversions.get(item).get("amount_per_contact", 0)
-            amount_per_view = conversions.get(item).get("amount_per_view", 0)
+    top_items = week_report_data.get("top")
+    if top_items is not None:
+        for item in top_items:
+            itemTitle = top_items.get(item).get("itemTitle", 0)
+            uniqContacts = top_items.get(item).get("uniqContacts", 0)
+            uniqFavorites = top_items.get(item).get("uniqFavorites", 0)
+            uniqViews = top_items.get(item).get("uniqViews", 0)
+            coast = top_items.get(item).get("coast", 0)
+            amount_per_contact = top_items.get(item).get("amount_per_contact", 0)
+            amount_per_view = top_items.get(item).get("amount_per_view", 0)
 
             statistics_text = (f"\n Объявление №{item}\n"
                                f"Название - {itemTitle}\n")
@@ -57,7 +71,6 @@ async def get_week_report_text(message: Message):
                 statistics_text += f"Цена контакта - {amount_per_contact}\n"
             if amount_per_view > 0:
                 statistics_text += f"Цена просмотра - {amount_per_view}\n"
-
             statistics_total += statistics_text
     return text + statistics_total
 
