738871ca4ffd8d379b1da07aa157ac171d32c7ab
rauf <abdurauf.dev@gmail.com>
2024-06-01 17:50:47 +0500

    перед деплоем V1 1)чистые миграции 2)настройки логирования сентри

--- base/urls.py
+++ base/urls.py
@@ -3,9 +3,13 @@ from django.urls import include
 from django.urls import path
 
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("oauth/", include("avito_account.urls")),
     path("conversion/", include("conversion.urls")),
-    path("messaging/", include("messaging.urls")),
+    path('sentry-debug/', trigger_error),
 ]
