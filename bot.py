ba16bf4eceb92bb5acd5c01c7a25dfd81bf8a04e
rauf <abdurauf.dev@gmail.com>
2024-06-03 15:02:09 +0500

    scheduler fixes

--- bot.py
+++ bot.py
@@ -74,7 +74,7 @@ async def main() -> None:
     cleaner = Cleaner(limit=100)
     dp.update.middleware(CleanerMiddleware(cleaner))
 
-    await scheduler_setup(scheduler, bot)
+    await scheduler_setup(scheduler)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
