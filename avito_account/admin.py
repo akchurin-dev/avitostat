75459cde435fe138700b8838f69d5867b0da242b
rauf <abdurauf.dev@gmail.com>
2024-06-25 16:09:24 +0500

    Разделение по аккаунтам

--- avito_account/admin.py
+++ avito_account/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.admin import site
-from avito_account.models import AvitoAccount, Item
-from conversion.models import ServiceType, Operation
+
+from avito_account.models import AvitoAccount
+from conversion.models import Operation
 
 
 class OperationInline(admin.TabularInline):
@@ -15,7 +16,15 @@ class ItemAdmin(admin.ModelAdmin):
     inlines = [OperationInline, ]
 
 
-site.register(AvitoAccount)
+class AvitoAccountAdmin(admin.ModelAdmin):
+    list_display = ('company', 'name', 'telegram_id', 'phone', 'profile_url')
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.filter(company_id=request.user.pk)
+
+
+site.register(AvitoAccount, AvitoAccountAdmin)
 # site.register(Item, ItemAdmin)
 # site.register(ServiceType)
 # site.register(Operation)
--- base/settings.py
+++ base/settings.py
@@ -117,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
@@ -169,3 +169,37 @@ if ENVIRONMENT == 'PRODUCTION':
         # We recommend adjusting this value in production.
         profiles_sample_rate=1.0,
     )
+
+
+JET_THEMES = [
+    {
+        'theme': 'default', # theme folder name
+        'color': '#47bac1', # color of the theme's button in user menu
+        'title': 'Default' # theme title
+    },
+    {
+        'theme': 'green',
+        'color': '#44b78b',
+        'title': 'Green'
+    },
+    {
+        'theme': 'light-green',
+        'color': '#2faa60',
+        'title': 'Light Green'
+    },
+    {
+        'theme': 'light-violet',
+        'color': '#a464c4',
+        'title': 'Light Violet'
+    },
+    {
+        'theme': 'light-blue',
+        'color': '#5EADDE',
+        'title': 'Light Blue'
+    },
+    {
+        'theme': 'light-gray',
+        'color': '#222',
+        'title': 'Light Gray'
+    }
+]
