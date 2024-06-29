0c9640e3219503d39281ad7acc58c3f2510be622
rauf <abdurauf.dev@gmail.com>
2024-06-29 17:04:26 +0500

    OPENAI ver 2

--- messaging/utils_open_ai.py
+++ messaging/utils_open_ai.py
@@ -1,10 +1,12 @@
-import os
 import openai
 from dotenv import load_dotenv
 
+from openai import OpenAI
+import os
+
 load_dotenv()
-OPENAI_SECRET_KEY = os.getenv('OPENAI_SECRET_KEY')
-openai.api_key = OPENAI_SECRET_KEY
+MODEL = "gpt-4o"
+client = OpenAI(api_key=os.environ.get("OPENAI_SECRET_KEY"))
 
 
 def compare_messages_for_ai(chats_with_raw_messages: list):
@@ -16,46 +18,37 @@ def compare_messages_for_ai(chats_with_raw_messages: list):
             if message['direction'] == 'in':
                 compared_messages[-1].get('messages').append({"role": "user", "content": message['content']['text']})
             elif message['direction'] == 'out':
-                compared_messages[-1].get('messages').append({"role": "assistant", "content": message['content']['text']})
+                compared_messages[-1].get('messages').append(
+                    {"role": "assistant", "content": message['content']['text']})
     return compared_messages
 
 
 # Функция для общего анализа переписки
 def analyze_overall_conversation(chats_with_compared_messages: list):
     chats_analyze = []
-    for chat in chats_with_compared_messages:
+    for chat in chats_with_compared_messages[:5]:     #TODO CLEAR THIS
         chat_text = "\n".join([message.get('content') + message.get('role') for message in chat.get('messages')])
         prompt = (f"Оцените эту переписку с точки зрения клиентского обслуживания."
                   f"Переписка:\n{chat_text}\n"
+                  f"Необходимо выявить факты непрофессионального общения со стороны менеджера."
+                  f"Например невовлеченность в сделку, или холодное общение"
+                  f"Нежелание помочь клиенту  найти интересующую информацию и тп"
+                  f"НЕ БЕРИ в расчёт такие аспекты как - "
+                  f"1) то что менеджер просит контактный номер"
+                  f"2) игнорируются системные сообщения о запрете перехода в другие месенджеры"
                   f"Почему данная переписка может быть плохим примером?")
-        response = openai.Completion.create(
-            model="gpt-4",
-            prompt=prompt,
-            max_tokens=500,
-            n=1,
-            stop=None,
+
+        ### NEW GENERATION
+        prompt_with_chats = prompt + f"вот сама переписка - {chat_text}"
+        completion = client.chat.completions.create(
+            model=MODEL,
+            messages=[
+                {"role": "system", "content": prompt_with_chats},
+            ],
             temperature=0.5
         )
-        analysis = response.choices[0].text.strip()
         chats_analyze.append({
             "id": chat.get('id'),
-            "analysis": analysis,
+            "analysis": completion.choices[0].message.content
         })
     return chats_analyze
-
-# Анализируем переписку в целом
-# overall_analysis = analyze_overall_conversation(messages)
-# print("Общий анализ переписки:")
-# print(overall_analysis)
-
-# from openai import AsyncOpenAI
-#
-# client = AsyncOpenAI()
-#
-# response = await client.chat.completions.create(
-#             model="gpt-4",
-#             messages=messages,
-#             tools=functions,
-#             temperature=0.0,
-#             tool_choice=None
-#         )
