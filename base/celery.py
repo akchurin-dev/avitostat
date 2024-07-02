6ebd49959df11e8bb04184b9c7983ee8cba38f0d
rauf <abdurauf.dev@gmail.com>
2024-07-02 19:37:17 +0500

    celery first task work

--- base/celery.py
+++ base/celery.py
@@ -1,5 +1,3 @@
-# celery.py
-
 import os
 from celery import Celery
 
@@ -7,10 +5,22 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
 
 # Создаем экземпляр Celery
-app = Celery('avitostat')
+celery_app = Celery('avitostat')
 
 # Загружаем конфигурацию из настроек Django
-app.config_from_object('django.conf:settings', namespace='CELERY')
+celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Автоматически находим и регистрируем все задачи (tasks) из приложений Django
-app.autodiscover_tasks()
+celery_app.autodiscover_tasks()
+
+
+# Определяем периодические задачи
+@celery_app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Пример периодической задачи, которая запускается каждые 10 секунд
+    sender.add_periodic_task(10.0, test.s('hello'), name='add every 10 seconds')
+
+
+@celery_app.task
+def test(arg):
+    print(arg)
--- base/settings.py
+++ base/settings.py
@@ -219,15 +219,13 @@ else:
         'TOKEN': os.getenv('TELEGRAM_BOT_TOKEN')
     }
 
-
-#CELERY settings
-#CELERY settings
-#CELERY settings
+# CELERY settings
+# CELERY settings
+# CELERY settings
 
 # Добавляем настройки для Celery
-CELERY_BROKER_URL = 'redis://localhost:6379/0'  # URL для подключения к Redis
-
-# Настройки Celery
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
