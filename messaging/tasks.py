731b7ca6c4e85d984811118e322a69fdeb8a332e
rauf <abdurauf.dev@gmail.com>
2024-07-03 11:28:36 +0500

    before installing ASGIREF

--- messaging/tasks.py
+++ messaging/tasks.py
@@ -1,6 +1,13 @@
 from celery import shared_task
 from telegram_bot import bot
 
+from avito_account.models import AvitoAccount
+from messaging.utils_bad_messaging_report import get_bad_messaging_week_report_pdf
+
+
 @shared_task
 def send_test_message():
-    bot.send_raw(chat_id="-1002061228822", text="TEST TEXT")
+    # pdf = await get_bad_messaging_week_report_pdf(84804440)
+    all_avito_accounts = AvitoAccount.objects.filter(company__is_active=True, telegram_id__isnull=False)
+    for avito_account in all_avito_accounts:
+        bot.send_raw(chat_id=avito_account.telegram_id, text="TEST TEXT")
--- messaging/urls.py
+++ messaging/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from messaging.views import DurationWeekStatisticsView, BadMessagingWeekReportView
+from messaging.views import DurationWeekStatisticsView
 from messaging.views_test import TelegramSenderTestView
 
 urlpatterns = [
     path('week_report/<str:telegram_id>/', DurationWeekStatisticsView.as_view(), name='duration_week_stats'),
     # better to be duration_week_report instead week_report
-    path('bad_messagging_week_report/<str:avito_accounts_id>/', BadMessagingWeekReportView.as_view(), name='bad_messaging_week'),
+    # path('bad_messagging_week_report/<str:avito_accounts_id>/', BadMessagingWeekReportView.as_view(), name='bad_messaging_week'),
     path('send_test_message', TelegramSenderTestView.as_view(), name='bad_messaging_week'),
 ]
