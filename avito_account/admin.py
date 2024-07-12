0a86085656df0fd11a202a4020ef23bc964202ea
rauf <abdurauf.dev@gmail.com>
2024-07-12 12:40:49 +0500

    temp for admin.py

--- avito_account/admin.py
+++ avito_account/admin.py
@@ -20,7 +20,7 @@ class ItemAdmin(admin.ModelAdmin):
 class AvitoAccountAdmin(admin.ModelAdmin):
     list_display = ('company', 'name', 'telegram_id', 'phone', 'profile_url')
     readonly_fields = ('id',)
-    exclude = ('access_token', 'refresh_token')
+    # exclude = ('access_token', 'refresh_token')
 
     def get_queryset(self, request):
         if request.user.is_superuser:
