52055cd63588277059281fcbbbc811b4ec85476a
rauf <abdurauf.dev@gmail.com>
2024-07-12 18:45:58 +0500

    celery-delay added testing

--- messaging/views_bad_messaging.py
+++ messaging/views_bad_messaging.py
@@ -6,5 +6,5 @@ from messaging.tasks import bad_messaging_week_report_async
 
 class BadMessagingWeekReportAllView(View):
     async def get(self, request, *args, **kwargs):
-        await bad_messaging_week_report_async()
+        await bad_messaging_week_report_async.delay()
         return JsonResponse(status=200, data={"success": "Всё прошло успешно"})
