4d62d69eba87d41f5a3bafc5e831e3deb9ae261d
raufakchurin <abdurauf.dev@gmail.com>
2024-07-16 12:11:39 +0500

    ver 1 for testing

--- messaging/tasks.py
+++ messaging/tasks.py
@@ -17,10 +17,19 @@ def bad_messaging_week_report_async_task():
 
 
 @shared_task
-async def bad_messaging_week_report_async(test_from_prod: bool = False):
-    all_avito_accounts = await sync_to_async(list)(
-        AvitoAccount.objects.filter(company__is_active=True, telegram_id__isnull=False)
-    )
+async def bad_messaging_week_report_async(test_from_prod: bool = False, only_for_users=None):
+    #  Queryset filtering logic
+    if only_for_users is None:
+        all_avito_accounts = await sync_to_async(list)(AvitoAccount.objects.filter(
+                company__is_active=True,
+                telegram_id__isnull=False))
+    else:
+        all_avito_accounts = await sync_to_async(list)(AvitoAccount.objects.filter(
+                id__in=only_for_users,
+                company__is_active=True,
+                telegram_id__isnull=False))
+
+    # CORE logic
     for avito_account in all_avito_accounts:
         try:
             pdf_path = await get_bad_messaging_week_report_pdf(avito_account.id)
--- messaging/views_bad_messaging.py
+++ messaging/views_bad_messaging.py
@@ -1,10 +1,16 @@
 from django.http import JsonResponse
 from django.views import View
-
 from messaging.tasks import bad_messaging_week_report_async
 
 
 class BadMessagingWeekReportAllView(View):
     async def get(self, request, *args, **kwargs):
         await bad_messaging_week_report_async.delay()
+        return JsonResponse(status=200, data={"success": "Всё прошло успешно"})
+
+
+class BadMessagingWeekReportIndividualView(View):
+    #TODO добавить извлечение айдишника из урла и добавление в фильтр
+    async def get(self, request, *args, **kwargs):
+        await bad_messaging_week_report_async(only_for_users=[203199629, 203199629])
         return JsonResponse(status=200, data={"success": "Всё прошло успешно"})
