5308c65a8df97d6826143aae0a3fe14f66f85945
raufakchurin <abdurauf.dev@gmail.com>
2024-07-17 10:35:56 +0500

    bottom functionals COMPLETED

--- messaging/urls.py
+++ messaging/urls.py
@@ -6,6 +6,6 @@ from messaging.views_bad_messaging import BadMessagingWeekReportAllView, BadMess
 urlpatterns = [
     path('week_report/<str:avito_id>/', DurationWeekStatisticsView.as_view(), name='duration_week_stats'),
     path('bad_messaging_week_report_all_to_users/', BadMessagingWeekReportAllView.as_view(), name='bad_messaging_week_report_all'),
-    path('bad_messaging_week_report_individual/<str:avito_id>', BadMessagingWeekReportIndividualView.as_view(), name='bad_messaging_week_individual'),
+    path('bad_messaging_week_report_individual/<int:object_id>/', BadMessagingWeekReportIndividualView.as_view(), name='bad_messaging_week_individual'),
     # better to be duration_week_report instead week_report
 ]
--- messaging/views_bad_messaging.py
+++ messaging/views_bad_messaging.py
@@ -1,6 +1,6 @@
+from messaging.tasks import bad_messaging_week_report_async
 from django.http import JsonResponse
 from django.views import View
-from messaging.tasks import bad_messaging_week_report_async
 
 
 class BadMessagingWeekReportAllView(View):
@@ -10,7 +10,7 @@ class BadMessagingWeekReportAllView(View):
 
 
 class BadMessagingWeekReportIndividualView(View):
-    #TODO добавить извлечение айдишника из урла и добавление в фильтр
     async def get(self, request, *args, **kwargs):
-        await bad_messaging_week_report_async(only_for_users=[145213826])
+        object_id = kwargs.get('object_id')
+        await bad_messaging_week_report_async(only_for_users=[object_id])
         return JsonResponse(status=200, data={"success": "Всё прошло успешно"})
