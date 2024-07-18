c65358e5f8583ea08d6f4a0820267fa8acce665b
raufakchurin <abdurauf.dev@gmail.com>
2024-07-18 20:40:52 +0500

    logging added

--- base/settings.py
+++ base/settings.py
@@ -29,7 +29,7 @@ ENVIRONMENT = os.getenv('ENVIRONMENT')
 if ENVIRONMENT == 'DEVELOPMENT':
     DEBUG = True
 else:
-    DEBUG = True
+    DEBUG = False
 
 ALLOWED_HOSTS = ["172.22.0.2", "localhost", "127.0.0.1", "45.12.238.229", "*", "avitostata.ru"]
 
@@ -175,32 +175,32 @@ if ENVIRONMENT == 'PRODUCTION':
         profiles_sample_rate=1.0,
     )
 
-    # LOGGING = {
-    #     'version': 1,
-    #     'disable_existing_loggers': False,
-    #     'handlers': {
-    #         'sentry': {
-    #             'level': 'ERROR',
-    #             'class': 'sentry_sdk.integrations.logging.EventHandler',
-    #         },
-    #         'console': {
-    #             'level': 'DEBUG',
-    #             'class': 'sentry_sdk.integrations.logging.EventHandler',
-    #         },
-    #     },
-    #     'loggers': {
-    #         'django': {
-    #             'handlers': ['console', 'sentry'],
-    #             'level': 'DEBUG',
-    #             'propagate': True,
-    #         },
-    #         'celery': {
-    #             'handlers': ['console', 'sentry'],
-    #             'level': 'DEBUG',
-    #             'propagate': True,
-    #         },
-    #     },
-    # }
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'sentry': {
+                'level': 'ERROR',
+                'class': 'sentry_sdk.integrations.logging.EventHandler',
+            },
+            'console': {
+                'level': 'DEBUG',
+                'class': 'sentry_sdk.integrations.logging.EventHandler',
+            },
+        },
+        'loggers': {
+            'django': {
+                'handlers': ['console', 'sentry'],
+                'level': 'DEBUG',
+                'propagate': True,
+            },
+            'celery': {
+                'handlers': ['console', 'sentry'],
+                'level': 'DEBUG',
+                'propagate': True,
+            },
+        },
+    }
 
 #TODO django-redis-aiogram sender SETTINGS
 #TODO django-redis-aiogram sender SETTINGS
