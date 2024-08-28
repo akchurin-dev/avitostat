6cb8e7f80c7ad988373cfab811c33bed68fffb66
raufakchurin <abdurauf.dev@gmail.com>
2024-08-28 17:20:53 +0500

    colors added

--- deep_tests/views.py
+++ deep_tests/views.py
@@ -28,7 +28,7 @@ class BadMessagingWeekReportTestView(View):
 
 
 class BadMessagingWeekReportAllTestView(View):
-    async def get(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         bad_messaging_week_report_async_task.delay(test_from_prod=True)
         # TODO Придумать нормальные условия для 200 и других статусов, тк сейчас всегда 200
         return HttpResponse(status=200)
--- messaging/bad_mes_report/statistics/statistics_by_criteria_utils.py
+++ messaging/bad_mes_report/statistics/statistics_by_criteria_utils.py
@@ -15,15 +15,33 @@ async def get_statistics_by_criteria_splitted_by_managers(filtered_chats_only_wi
     return grouped_chats
 
 
+async def get_color_description(result: dict, color="red", text="ПЛОХО"):
+    percentage = result.get("positive_chats") / result.get("total_chats")
+    if 0.33 >= percentage > 0:
+        color = "#C14D3D"
+        text = "плохо"
+    elif 0.66 >= percentage > 0.33:
+        color = "#E4A03B"
+        text = "средне"
+    elif 1 >= percentage > 0.66:
+        color = "#73C356"
+        text = "отлично"
+
+    return {
+        "text": text,
+        "color": color,
+    }
+
+
 async def get_statistics_by_criteria(actual_chats_with_messages: list):
-    result = {}
+    results = {}
     for chat in actual_chats_with_messages:
         analyze_by_criteria = chat.get("analyze_by_criteria", None)
         # Initialize counters
         if analyze_by_criteria is not None:
-            if len(result) == 0:
+            if len(results) == 0:
                 for key, value in analyze_by_criteria.items():
-                    result[key] = {
+                    results[key] = {
                         "positive_chats": 0,
                         "total_chats": len(actual_chats_with_messages),
                         "criterion_name": value.get("criterion"),
@@ -32,7 +50,10 @@ async def get_statistics_by_criteria(actual_chats_with_messages: list):
             # # Feel positive criteria counter
             for k, v in analyze_by_criteria.items():
                 if v.get("meets_criterion"):
-                    result[k]["positive_chats"] += 1
+                    results[k]["positive_chats"] += 1
 
-    result_converted_to_list = list(result.values())
+            for result in results.values():
+                result["description"] = await get_color_description(result)
+
+    result_converted_to_list = list(results.values())
     return result_converted_to_list
