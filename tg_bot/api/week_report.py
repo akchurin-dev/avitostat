aba71f126d0f52c4787bd91a8a045b9bca1d9507
Rauf Akchurin <74125860+RaufAkchurin@users.noreply.github.com>
2024-09-10 17:00:20 +0500

    first (#17)

--- tg_bot/api/week_report.py
+++ tg_bot/api/week_report.py
@@ -43,13 +43,13 @@ def get_duration_report_by_avito_id(avito_id: int):
         raise HTTPException(status_code=response.status_code, detail=response.text)
 
 
-def get_bad_messaging_week_report_all_to_users():
+def get_pdf_report_all_to_users():
     url = f"{BASE_URL}/messaging/bad_messaging_week_report_all_to_users/"
     response = requests.get(url=url)
     return response.json()
 
 
-def get_bad_messaging_week_report_all_to_admin():
+def get_pdf_report_all_to_admin():
     url = f"{BASE_URL}/deep_tests/bad_messaging_week_report_all_to_admin/"
     response = requests.get(url=url)
     return response.json()
