ca8fca6d9dec4d6bd60cb11d5ca56eb70cec20b2
raufakchurin <abdurauf.dev@gmail.com>
2024-09-13 07:24:33 +0500

    рефакторинг отправки ТХТ отчётов

--- conversion/tasks.py
+++ conversion/tasks.py
@@ -1,14 +1,10 @@
 import os
 from asgiref.sync import async_to_sync, sync_to_async
+from django.utils import timezone
 from telegram_bot import bot
+from avito_account.models import SendingCampaign, AvitoAccount, SendingReport
 from base.celery import celery_app
 from conversion.utils_from_aiogram import get_week_report_text, get_avito_account_all_ids
-from exceptions import HTTPException
-
-
-# @celery_app.task(name='conversion.tasks.send_txt_week_report_individual_async')
-# def send_txt_week_report_individual_async_task(telegram_chat_id=None, test_from_prod=False):
-#     async_to_sync(send_txt_week_report_individual_async)(telegram_chat_id=telegram_chat_id, test_from_prod=test_from_prod)
 
 
 async def send_txt_week_report_individual_async(telegram_chat_id: int, test_from_prod: bool = False):
@@ -37,12 +33,36 @@ def send_text_report_all_async_task(test_from_prod=False, only_for_users=None):
 
 
 async def send_text_report_all_async(test_from_prod: bool = False, only_for_users=None):
+    campaign = await SendingCampaign.objects.acreate(
+        name='weekly',
+        test_from_prod=test_from_prod,
+        sending_type=SendingCampaign.TEXT,
+        created_at=timezone.now(),
+    )
+
     if only_for_users is not None:
         avito_account_ids = only_for_users
     else:
         avito_account_ids = await get_avito_account_all_ids()
+
+    # queryset = await sync_to_async(AvitoAccount.objects.filter)(id__in=avito_account_ids)
     for account_id in avito_account_ids:
+        avito_account = await AvitoAccount.objects.aget(telegram_id=account_id)
         try:
             await send_txt_week_report_individual_async(int(account_id), test_from_prod=test_from_prod)
+            await SendingReport.objects.acreate(
+                avito_account=avito_account,
+                campaign=campaign,
+                success=True,
+                pdf_path=None,
+                timestamp=timezone.now()
+            )
         except Exception as e:
-            raise HTTPException(status_code=500, detail=str(e))
+            await SendingReport.objects.acreate(
+                avito_account=avito_account,
+                campaign=campaign,
+                success=False,
+                error_message=str(e)[:50],
+                timestamp=timezone.now(),
+                pdf_path=None
+            )
