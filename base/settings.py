402f299e02f4b99a293f628bca311c3e0c890834
raufakchurin <abdurauf.dev@gmail.com>
2024-09-23 11:49:42 +0500

    UTC везде

--- base/settings.py
+++ base/settings.py
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
 LANGUAGE_CODE = 'ru-RU'
-TIME_ZONE = 'Europe/Moscow'
+TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
