4c4af795e07db7ddf930fd61590d24b90d1da187
raufakchurin <abdurauf.dev@gmail.com>
2024-10-06 10:22:45 +0500

    временно

--- payments/views.py
+++ payments/views.py
@@ -1,10 +1,21 @@
 from django.http import HttpResponseRedirect
+from django.shortcuts import render
 from .services import YookassaService
 from django.views.generic import TemplateView
 
 
 class PaymentView(TemplateView):
     template_name = 'payment.html'  # Путь к вашему шаблону payment.html
+
+    def get(self, request, *args, **kwargs):
+        # Получаем ID текущего пользователя
+        user_id = request.user.id
+        # Формируем контекст с ID пользователя
+        context = {
+            'current_user_id': user_id
+        }
+        # Рендерим шаблон с переданным контекстом
+        return render(request, self.template_name, context)
 
 
 def create_payment(request):
