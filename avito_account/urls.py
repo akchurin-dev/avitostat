449e9123fe00257c5150651e6b0a82405355c7de
raufakchurin <abdurauf.dev@gmail.com>
2024-10-20 19:06:15 +0500

    добавил комменты

--- avito_account/urls.py
+++ avito_account/urls.py
@@ -3,7 +3,5 @@ from .views import CallbackView, terms_of_service
 
 urlpatterns = [
     path('callback/', CallbackView.as_view(), name='callback'),
-    path('terms-of-service/', terms_of_service, name='terms_of_service'),
+    path('terms-of-service/', terms_of_service, name='terms_of_service'),  # Пользовательское соглашение
 ]
-
-
