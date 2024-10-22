33b608f170d265c58a1875f9dda27a9a4af85050
raufakchurin <abdurauf.dev@gmail.com>
2024-10-22 09:39:18 +0500

    divizion by zero

--- payments/middleware.py
+++ payments/middleware.py
@@ -16,7 +16,7 @@ class UserProfileMiddleware(MiddlewareMixin):
                 active_accounts = AvitoAccount.objects.filter(created_by=request.user).count()
             response.context_data = response.context_data or {}
             response.context_data['balance'] = user_profile.balance
-            response.context_data['days_left'] = round(int(user_profile.balance // 500) / active_accounts * 7)
+            response.context_data['days_left'] = round(int(user_profile.balance // 500) / (active_accounts or 1) * 7)
             response.context_data['active_accounts'] = active_accounts
             if settings.ENVIRONMENT == "DEVELOPMENT":  # TODO change script filling test DB and remove this code
                 response.context_data['active_accounts'] = 31
