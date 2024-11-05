5e367815ab8e7bde20dca91835676ee7ee85e09d
raufakchurin <abdurauf.dev@gmail.com>
2024-11-05 14:41:03 +0500

    прочитываются сообщения

--- chat_bot/tasks.py
+++ chat_bot/tasks.py
@@ -2,7 +2,7 @@ import asyncio
 from base.celery import celery_app
 from avito_account.models.models import AvitoAccount
 from chat_bot.ai_utils import ai_answer_assist
-from chat_bot.api.core import send_message_to_avito
+from chat_bot.api.core import send_message_to_avito, read_chat
 from chat_bot.models import AiChatBot
 from messaging.api import get_chats_messages
 
@@ -25,6 +25,7 @@ async def process_webhook_async(user_id, data):
 
         chat_with_messages = await get_chats_messages(avito_account, chats=[{"id": chat_id}])
         if author_id != user_id and chat_bot.is_active:  # Проверка авторства
+            await read_chat(avito_account, user_id, chat_id)
             ai_answer = await ai_answer_assist(chat_bot, chat_with_messages)
             if ai_answer:
                 ai_answer += f" answer for {content}"
