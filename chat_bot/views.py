6657d3cdc6f7f994ad45f7222eaa460a3fda23df
raufakchurin <abdurauf.dev@gmail.com>
2024-11-17 10:17:54 +0500

    refactoring

--- chat_bot/views.py
+++ chat_bot/views.py
@@ -27,37 +27,41 @@ async def check_chat_bot_scheduler(chat_bot: AiChatBot):
 
 @method_decorator(csrf_exempt, name='dispatch')
 class WebhookInboxView(View):
+    async def revoke_old_tasks(self, chat_id: str):
+        current_time = now().astimezone(moscow_tz)
+        last_2_hours = current_time - datetime.timedelta(hours=2)
+        old_tasks = await sync_to_async(list)(ChatBotTask.objects.filter(
+            chat_id=chat_id,
+            created_at__gt=last_2_hours))
+        if old_tasks:
+            for old_task in old_tasks:
+                old_task_id = f"ai_answer_{old_task.message_id}"
+                existing_task = AsyncResult(old_task_id)
+                if existing_task and existing_task.status == "PENDING":
+                    existing_task.revoke(terminate=True)
+                    # logger.info(f"Task {old_task_id} revoked before launching")
+
+    async def prepare_data(self):
+        message_id = self.data.get('payload').get('value').get('id')
+        chat_id = self.data.get("payload").get("value").get("chat_id")
+        author_id = self.data.get("payload").get("value").get("author_id")
+        incoming_message = self.data.get("payload").get("value").get("content").get("text")
+        time_to_work = await check_chat_bot_scheduler(self.chat_bot)
+        return message_id, chat_id, author_id, incoming_message, time_to_work
+
     async def post(self, request, *args, **kwargs):
         decoded_string = request.body.decode('utf-8')
-        data = json.loads(decoded_string)
+        self.data = json.loads(decoded_string)
 
-        user_id = data.get("payload").get("value").get("user_id")
+        user_id = self.data.get("payload").get("value").get("user_id")
         avito_account = await AvitoAccount.objects.aget(id=user_id)
-        chat_bot = await AiChatBot.objects.aget(avito_account=avito_account)
+        self.chat_bot = await AiChatBot.objects.aget(avito_account=avito_account)
         await avito_account.update_refresh_token_async()
 
-        if data.get("payload").get("type") == "message":
-            message_id = data.get('payload').get('value').get('id')
-            chat_id = data.get("payload").get("value").get("chat_id")
-            author_id = data.get("payload").get("value").get("author_id")
-            incoming_message = data.get("payload").get("value").get("content").get("text")
-            time_to_work = await check_chat_bot_scheduler(chat_bot)
-
-            if author_id != user_id and chat_bot.is_active and time_to_work:
-                current_time = now().astimezone(moscow_tz)
-                last_2_hours = current_time - datetime.timedelta(hours=2)
-
-                old_tasks = await sync_to_async(list)(ChatBotTask.objects.filter(
-                                                    chat_id=chat_id,
-                                                    created_at__gt=last_2_hours, ))
-                if old_tasks:
-                    for old_task in old_tasks:
-                        old_task_id = f"ai_answer_{old_task.message_id}"
-                        existing_task = AsyncResult(old_task_id)
-                        if existing_task and existing_task.status == "PENDING":
-                            existing_task.revoke(terminate=True)
-                            # logger.info(f"Task {old_task_id} revoked before launching")
-
+        if self.data.get("payload").get("type") == "message":
+            message_id, chat_id, author_id, incoming_message, time_to_work = await self.prepare_data()
+            if author_id != user_id and self.chat_bot.is_active and time_to_work:
+                await self.revoke_old_tasks(chat_id)
                 new_task, created = await ChatBotTask.objects.aget_or_create(
                     chat_id=chat_id,
                     message_id=message_id,
@@ -67,9 +71,8 @@ class WebhookInboxView(View):
 
                 if created:
                     await ai_answer_sender(
-                        avito_account.id, user_id, chat_id, chat_bot.id, incoming_message, new_task.message_id,
+                        avito_account.id, user_id, chat_id, self.chat_bot.id, incoming_message, new_task.message_id,
                     )
-
         return JsonResponse({"status": "ok"}, status=200)
 
 
