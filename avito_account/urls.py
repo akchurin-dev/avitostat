c59aeca18cfedf327db077d3edb9723a054a447b
raufakchurin <abdurauf.dev@gmail.com>
2024-12-04 08:57:08 +0500

    штрихи

--- avito_account/urls.py
+++ avito_account/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import CallbackView, terms_of_service, TestBalanceView
+from .views import CallbackView, terms_of_service
 
 urlpatterns = [
     path('callback/', CallbackView.as_view(), name='callback'),
-    path('balance/', TestBalanceView.as_view(), name='balance'),
     path('terms-of-service/', terms_of_service, name='terms_of_service'),  # Пользовательское соглашение
 ]
