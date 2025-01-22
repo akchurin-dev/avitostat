90b19df8d8ba1f61c369c8dd1f2c4c56b38af855
raufakchurin <abdurauf.dev@gmail.com>
2025-01-22 16:25:26 +0500

    разрешенные хосты

--- base/settings.py
+++ base/settings.py
@@ -39,7 +39,7 @@ DB_NAME = os.getenv('DB_NAME')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS = [
-    "localhost", "127.0.0.1", "avitostata.ru",
+    "localhost", "127.0.0.1", "www.avitostata.ru",
 
     #Yookassa webhook
     "185.71.76.0/27", "185.71.77.0/27", "77.75.153.0/25", "77.75.156.11",
