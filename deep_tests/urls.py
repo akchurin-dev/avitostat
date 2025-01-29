2408d90c98e70e78f413bfd61ac64b8a30d153dc
raufakchurin <abdurauf.dev@gmail.com>
2025-01-29 21:39:37 +0500

    ROLLBAR sentry tester

--- deep_tests/urls.py
+++ deep_tests/urls.py
@@ -3,17 +3,21 @@ from django.urls import path
 
 from deep_tests.tasks import division_by_zero_task
 from deep_tests.views import BadMessagingWeekReportTestView, TelegramSenderTestView, TelegramDocumentSenderTestView, \
-    BadMessagingWeekReportAllTestView
+    BadMessagingWeekReportAllTestView, DivizionByZeroCeleryTaskTestView
+
 
 @shared_task
 def trigger_error():
     division_by_zero = 1 / 0
+
 
 urlpatterns = [
     path('bad_messagging_week_report/<str:avito_accounts_id>/', BadMessagingWeekReportTestView.as_view(), name='bad_messaging_week'),
     path('bad_messaging_week_report_all_to_admin/', BadMessagingWeekReportAllTestView.as_view(), name='bad_messaging_week_all'),
     path('send_test_message', TelegramSenderTestView.as_view(), name='bad_messaging_week'),
     path('send_test_document', TelegramDocumentSenderTestView.as_view(), name='bad_messaging_week'),
-    path('glitchtip-debug/', trigger_error),
-    path('glitchtip-sentry-debug/', division_by_zero_task.delay()),
-    ]
+    path('rollbar-debug/', trigger_error),
+    path('rollbar-sentry-debug/', DivizionByZeroCeleryTaskTestView.as_view()),
+
+]
+
--- deep_tests/views.py
+++ deep_tests/views.py
@@ -2,6 +2,8 @@ from aiogram import types
 from django.http import HttpResponse
 from telegram_bot import bot
 from django.views import View
+
+from deep_tests.tasks import division_by_zero_task
 from messaging.tasks import bad_messaging_report_by_period, bad_messaging_week_report_async_task
 
 
@@ -31,4 +33,10 @@ class BadMessagingWeekReportAllTestView(View):
     def get(self, request, *args, **kwargs):
         bad_messaging_week_report_async_task.delay(test_from_prod=True)
         # TODO Придумать нормальные условия для 200 и других статусов, тк сейчас всегда 200
+        return HttpResponse(status=200)
+
+
+class DivizionByZeroCeleryTaskTestView(View):
+    def get(self, request, *args, **kwargs):
+        division_by_zero_task.delay()
         return HttpResponse(status=200)
