003ae954d640c4465be6a5da1a6bdadcbd0fa829
raufakchurin <abdurauf.dev@gmail.com>
2025-01-29 11:52:46 +0500

    Бисмиллях

--- deep_tests/urls.py
+++ deep_tests/urls.py
@@ -3,9 +3,13 @@ from django.urls import path
 from deep_tests.views import BadMessagingWeekReportTestView, TelegramSenderTestView, TelegramDocumentSenderTestView, \
     BadMessagingWeekReportAllTestView
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
 urlpatterns = [
     path('bad_messagging_week_report/<str:avito_accounts_id>/', BadMessagingWeekReportTestView.as_view(), name='bad_messaging_week'),
     path('bad_messaging_week_report_all_to_admin/', BadMessagingWeekReportAllTestView.as_view(), name='bad_messaging_week_all'),
     path('send_test_message', TelegramSenderTestView.as_view(), name='bad_messaging_week'),
     path('send_test_document', TelegramDocumentSenderTestView.as_view(), name='bad_messaging_week'),
-]
+    path('glitchtip-debug/', trigger_error),
+    ]
