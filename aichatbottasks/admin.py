f48ca337c7f48e1ffb793db201a86ca218913a81
Amir Mustafaev <amir.mustafaev@mentalsea.com>
2025-03-24 19:54:47 +0300

    delete service model from admin

--- aichatbottasks/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from aichatbottasks import models as aichatbottask_models
-
-
-@admin.register(aichatbottask_models.Task)
-class TaskAdmin(admin.ModelAdmin):
-    pass
