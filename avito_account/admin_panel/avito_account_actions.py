48d725aa1a68006235d4d0ef21c86b8934ef88e4
Amir Mustafaev <amir.mustafaev@mentalsea.com>
2025-05-15 15:08:01 +0300

    run text report throught celery

--- avito_account/admin_panel/avito_account_actions.py
+++ avito_account/admin_panel/avito_account_actions.py
@@ -81,7 +81,7 @@ run_pdf_all_test_from_prod_report.short_description = "ПДФ ВСЕМ ТЕСТ 
 def run_txt_report(self, request, queryset):
     avito_account_ids = list(queryset.values_list('id', flat=True))
     try:
-        send_text_report_all_async_task(only_for_users=avito_account_ids)
+        send_text_report_all_async_task.delay(only_for_users=avito_account_ids)
         self.message_user(request, "ТЕКСТОВЫЙ отчет успешно сгенерирован и отправлен.", level='success')
     except Exception as e:
         logger.error(f"ТЕКСТОВЫЙ ошибка при отправке отчета: {e}", exc_info=True)
