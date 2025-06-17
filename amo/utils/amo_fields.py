5fb918288f7a38f7c8277db6d8fcf6bbc4fd70b1
Amir Mustafaev <amir.mustafaev@mentalsea.com>
2025-06-17 13:55:59 +0300

    add logs

--- amo/utils/amo_fields.py
+++ amo/utils/amo_fields.py
@@ -110,10 +110,11 @@ def get_filled_fields(instance: amo_api.CustomFieldsContainer) -> list[amo_api.C
     return fields
 
 
-def get_empty_fields(instance: amo_api.CustomFieldsContainer) -> list[amo_api.CustomFieldValue] | None:
+def get_empty_fields(instance: amo_api.CustomFieldsContainer, *, tlogger: TraceLogger) -> list[amo_api.CustomFieldValue] | None:
     fields: list[amo_api.CustomFieldValue] = []
 
     if instance.custom_fields_values is None:
+        tlogger.info("Custom fields is None")
         return None
 
     for field_value in instance.custom_fields_values:
--- amo_a5client/utils/amo_a5_ai.py
+++ amo_a5client/utils/amo_a5_ai.py
@@ -30,7 +30,7 @@ def generate_answer(
 
     lead, contact = amo_leads.get_lead_contact_pair(chatbot.account, lead_id, tlogger=tlogger)
     all_fillable_fields = amo.models.FillableField.objects.filter(chatbot=chatbot)
-    unknown_fillable_fields = amo_ai.get_unknown_fillable_fields(all_fillable_fields, lead, contact)
+    unknown_fillable_fields = amo_ai.get_unknown_fillable_fields(all_fillable_fields, lead, contact, tlogger=tlogger)
     available_pipeline_statuses = amo_api.get_pipeline_statuses(amo_account, lead.pipeline_id, tlogger=tlogger)
 
     gpt_messages = _get_gpt_messages(
