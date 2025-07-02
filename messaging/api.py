45dd1096cd8c265c9a8aa0bfa65602491f1c1df9
Amir Mustafaev <amir.mustafaev@mentalsea.com>
2025-07-02 15:06:09 +0300

    edit get_voice_urls

--- messaging/api.py
+++ messaging/api.py
@@ -255,7 +255,7 @@ def get_voice_id_url_pairs(
     """ https://developers.avito.ru/api-catalog/messenger/documentation#operation/getVoiceFiles """
 
     action = f"/messenger/v1/accounts/{account.pk}/getVoiceFiles"
-    params = {"voice_ids": voices_ids}
+    params = {"voice_ids": ",".join(voices_ids)}
 
     response = avito_api_request(
         method="GET",
