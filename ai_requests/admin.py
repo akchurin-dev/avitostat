07948fdbfc3199b9d61aa62703a9e6302bbda7ba
Amir Mustafaev <amir.mustafaev@mentalsea.com>
2025-07-21 08:23:01 +0300

    airequests ordering

--- ai_requests/admin.py
+++ ai_requests/admin.py
@@ -6,3 +6,4 @@ import ai_requests.models
 @admin.register(ai_requests.models.AIRequest)
 class AIRequestAdmin(admin.ModelAdmin):
     list_display = ["tag", "model", "tokens_completion", "tokens_prompt", "created_at"]
+    ordering = ["-created_at"]
