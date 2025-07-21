c6b07c2d80648bba43968a967436f2abe79849eb
Amir Mustafaev <amir.mustafaev@mentalsea.com>
2025-07-21 08:20:28 +0300

    edit admin

--- ai_requests/admin.py
+++ ai_requests/admin.py
@@ -5,4 +5,4 @@ import ai_requests.models
 
 @admin.register(ai_requests.models.AIRequest)
 class AIRequestAdmin(admin.ModelAdmin):
-    list_display = ["model", "tokens_completion", "tokens_prompt", "created_at"]
+    list_display = ["tag", "model", "tokens_completion", "tokens_prompt", "created_at"]
